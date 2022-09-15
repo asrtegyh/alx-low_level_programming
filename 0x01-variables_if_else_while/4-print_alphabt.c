b603e460543b43a66616454e3034d87a3367c3b4
asrtegyh <wastonorganisation@gmail.com>
2022-09-15 02:53:28 -0700

    update,c print lower alphabets excluding q&c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ *  main - printout the alphabets in lowercase
+ *
+ * followed by newline, except q and e
+ *
+ *  Return: 0 Alwways success
+ *  
+ */
+int main (void)
+{
+	char chr = 'a';
+	 
+	while (chr <= 'z')
+	{
+		if (chr != 'e' && chr != 'q')
+		{
+			putchar(chr);
+		}
+		chr++
+	}
+	putchar('\n');
+	return (0);
+}

7f86437a93a85c771ea65e64382aa49b1a1f0ca2
asrtegyh <wastonorganisation@gmail.com>
2022-09-15 03:24:24 -0700

    updates, c prints single digit numbers of base"

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - printout of all digit numbers of base
+ * from 0 to 10,followed by newline
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d",n);
+	}
+	printf("\n")
+		return (0);
+}
+

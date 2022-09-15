9985576129ea4a8243a8700ee421e89614d53ff0
asrtegyh <wastonorganisation@gmail.com>
2022-09-15 02:20:53 -0700

    updates,c prints lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ *  main - printout of alphabet s in lowercase and then uppercase
+ *
+ *  followed by a new line
+ *
+ *  Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int chr;
+
+	for(chr='a'; chr='z' ;chr++)
+	{
+		putchar(chr);
+	}
+	for (chr='A'; chr='Z';chr++)
+	{
+		putchar(chr);
+	}
+	putchar('\n');
+	return (0);
+}
